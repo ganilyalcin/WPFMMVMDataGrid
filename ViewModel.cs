3fb539e49796a519bdf043494e4cf2a1a65501ac
ganilyalcin <ganilyalcin@gmail.com>
2017-06-07 21:47:11 +0300

    init
    init

---
+++ ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMMVMDataGrid
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<MissingDataModel> missingList;
+                
+        public List<MissingDataModel> MissingList
+        {
+            get
+            {
+                return this.missingList;
+            }
+
+            set
+            {
+                if (value == this.missingList)
+                {
+                    return;
+                }
+
+                this.missingList = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.MissingList));
+            }
+        }
+        
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class MissingDataModel
+    {
+        public int ZNumber { get; set; }
+        public string CashRegisterSerialNumber { get; set; }
+        public DateTime ZDateTime { get; set; }
+        public decimal DepartmentTotal { get; set; }
+        public decimal DepartmentTotalVat { get; set; }
+        public decimal CashAmount { get; set; }
+        public decimal CreditAmount { get; set; }
+    }
+}
